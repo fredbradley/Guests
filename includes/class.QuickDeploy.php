93d1583bdc36399b7155cba0956f09c842628c88
Fred Bradley <hello@fredbradley.co.uk>
2015-06-23 09:16:31 +0000

    added htaccess for page control

--- includes/class.QuickDeploy.php
+++ includes/class.QuickDeploy.php
@@ -16,9 +16,35 @@
 		}
 		
 		function get_template($template, array $parse_settings=array()) {
-			return $this->twig->render($template, $parse_settings);
+			$default_settings = array(
+				"footer_scripts" => $this->push_alerts_to_footer()
+			);
+			if (isset($parse_settings['footer_scripts'])) {
+				$default_settings['footer_scripts'] .= $parse_settings['footer_scripts'];
+			}
+			
+			$settings = array_merge($default_settings, $parse_settings);
+			return $this->twig->render($template, $settings);
 		}
 		
+		function guest_table_headers() {
+			return array(
+				"first_name" => "First Name",
+				"surname" => "Surname", 
+				"jobtitle" => "Job Title", 
+				"company" => "Company", 
+				"id" => "ID",
+				"edit" => "Blah"
+			);
+		}
+		function get_guests($event) {
+			$query = $this->db->query("SELECT * FROM `".$event."`");
+			$output = array();
+			while($guest = $query->fetch_object()):
+				$output[] = $guest;
+			endwhile;
+			return $output;
+		}
 		
 	
 	}
--- index.php
+++ index.php
@@ -1,11 +1,30 @@
-<?php include dirname(__FILE__).'/includes/appstrap.php';
+<?php 
+	include dirname(__FILE__).'/includes/appstrap.php';
 	$alerts = $engine->show_alerts();
-	var_dump($_SESSION);
 	$site_config = array(
 		"user_logged_in" => $engine->user_logged_in
 	);
+	if (isset($_GET['page_slug'])) {
+		$page = $_GET['page_slug'];
+	} else {
+		$page = "";
+	}
+	var_dump($page);
+	switch($page):
+		case "guestlist":
+			echo $engine->get_template(
+				"guest_table.twig", 
+				array(
+					"table_headers" => $engine->guest_table_headers(), 
+					"all_guests" => $engine->get_guests('ourwedding')
+				)
+			);
+		break;
+		default:
 //echo $engine->db->delete_event(4, "id");	
 //	echo $engine->db->add_event("Fred and Louise's Wedding", "ourwedding");
-	echo $engine->get_template("content.twig", array("config"=>$site_config, "title" => "Fred Bradley", "footer_scripts" => $engine->push_alerts_to_footer()));
+			echo $engine->get_template("content.twig", array("config"=>$site_config, "title" => "Fred Bradley"));
+		break;
+	endswitch;
 
 	
